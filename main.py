378e20adeff5a2f9ae0343f609dd3c2caa242f79
niravpatel6656 <npatel8705@conestogac.on.ca>
2024-04-28 12:52:03 -0400

    changed the credential of api and authentication

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import datetime as dt
 import json
 
 APP_ID = "f34647b8"
-API_KEY = "a343f7c656ab88847164a225d50a7e1a"
+API_KEY = "a34d6s5656gh23847rt56cha225d50a7e1a"
 
 natural_excercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
@@ -24,7 +24,7 @@ nutrition_facts = natural_exe_response.text
 
 data = json.loads(nutrition_facts)
 
-getting_sheet_data = requests.get(url="https://api.sheety.co/ce03a24dd7107449001e616b7fe7ac4c/myWorkouts/sheet1")
+getting_sheet_data = requests.get(url="https://api.sheety.co/ce56a24dd896549001e616b7fe7ac4c/myWorkouts/sheet1")
 
 current_date_and_time = dt.datetime.now()
 current_date = current_date_and_time.strftime("%d/%m/%Y")
@@ -47,10 +47,10 @@ put_data_parameters = {
 user_cred = requests.get('https://httpbin.org/basic-auth/user/pass', auth=('user', 'pass'))
 
 json_data = json.dumps(put_data_parameters)
-put_data_to_sheet = requests.post(url="https://api.sheety.co/ce03a24dd7107449001e616b7fe7ac4c/myWorkouts/sheet1",
+put_data_to_sheet = requests.post(url="https://api.sheety.co/ce03a4rty6107449001e616b7fe7ac4c/myWorkouts/sheet1",
                                   json=put_data_parameters,
                                   # auth=("nirav6656", "N.k.6656")
-                                  headers={"Authorization": "Basic bmlyYXY2NjU2Ok4uay42NjU2"}
+                                  headers={"Authorization": "Basic bmlyYXY245rfgOk4uay42NjU2"}
                                   )
 
 print(put_data_to_sheet.content)
