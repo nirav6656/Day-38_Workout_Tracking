5942597eae141d489eaa7cf8d7dc39d4f8f65d89
niravpatel6656 <npatel8705@conestogac.on.ca>
2024-04-28 00:37:45 -0400

    got data nutrix api

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import datetime as dt
+import json
 APP_ID = "f34647b8"
 API_KEY = "a343f7c656ab88847164a225d50a7e1a"
 
@@ -18,7 +20,35 @@ parameters = {
 
 
 natural_exe_response = requests.post(url=natural_excercise_endpoint,json=parameters,headers=headers)
-# print(natural_exe_response.text)
+nutrition_facts = natural_exe_response.text
+
+
+data = json.loads(nutrition_facts)
+print(data)
 
 getting_sheet_data = requests.get(url="https://api.sheety.co/ce03a24dd7107449001e616b7fe7ac4c/myWorkouts/workouts")
-print(getting_sheet_data.json())
+
+current_date_and_time = dt.datetime.now()
+current_date = current_date_and_time.strftime("%d/%m/%Y")
+current_time = current_date_and_time.strftime("%H:%M:%S")
+excercise_done = data["exercises"][0]["name"]
+excercise_duration = int(data["exercises"][0]["duration_min"])
+excercise_calories = int(data["exercises"][0]["nf_calories"])
+
+
+put_data_parameters = {
+    "My Workouts":
+        {"Date": current_date,
+         "Time": current_time,
+         "Exercise": excercise_done,
+         "Duration": excercise_duration,
+         "Calories": excercise_calories,
+         "id": 3}
+}
+
+
+put_data_to_sheet = requests.post(url="https://api.sheety.co/ce03a24dd7107449001e616b7fe7ac4c/myWorkouts/workouts",
+                                  json=put_data_parameters,
+                                  headers=headers)
+
+print(put_data_to_sheet.content)
